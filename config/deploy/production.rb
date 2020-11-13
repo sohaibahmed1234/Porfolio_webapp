c11e3953a14770ad203263357b4267e6e6ef54c3
sohaibahmed1234 <sohaib.ahmad@southvillesol.com>
2020-11-13 16:16:17 +0500

    change production.rb file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,12 +4,12 @@
 # You can define all roles on a single server, or split them:
 
 
-#role :app, %w{deploy@example.com}
-#role :web, %w{deploy@example.com}
-#role :db,  %w{deploy@example.com}
+role :app, %w{deploy@_}
+role :web, %w{deploy@_}
+role :db,  %w{deploy@_}
 
 
-#server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "_", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
