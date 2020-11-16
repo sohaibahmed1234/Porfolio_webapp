a1f1150df0d4e4b4a9302c94d5ac3f1283f38807
sohaibahmed1234 <sohaib.ahmad@southvillesol.com>
2020-11-16 16:42:27 +0500

    fix production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
 
   config.assets.compile = true
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
