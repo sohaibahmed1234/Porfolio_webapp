c038aa80fde737407482a0abfebf79774db5a3b6
sohaibahmed1234 <sohaib.ahmad@southvillesol.com>
2020-11-16 13:44:17 +0500

    fix production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
 
-
+  config.force_ssl = true
   # Suppress logger output for asset requests.
 
   config.assets.compile = true
