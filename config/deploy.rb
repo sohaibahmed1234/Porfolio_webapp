a3329e788b63dbaec0d445e6d0a2adb56d064eb6
sohaibahmed1234 <sohaib.ahmad@southvillesol.com>
2020-11-17 18:50:19 +0500

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,14 +65,6 @@ namespace :deploy do
     end
   end
 
-  namespace :deploy do
-    desc "reload the database with seed data"
-    task :seed do
-      run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
-    end
-  end
-
-
 
   desc 'Restart application'
   task :restart do
